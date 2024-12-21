339e221429889cbf8329091f7defeb968a008ca0
manlikexavy <chibokaxavier@gmail.com>
2024-12-21 17:42:28 +0100

    phone frame set up in progress

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -34,8 +34,10 @@ const Hero = () => {
         </div>
         <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
           <BackgroundDesign />
-          <div>
-            <PhoneFrame priority>gh</PhoneFrame>
+          <div className="-mx-4 h-[448px] px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
+            <PhoneFrame priority>
+hj
+            </PhoneFrame>
           </div>
         </div>
         <ExtraLogos />
